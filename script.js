14af1a80a090e029423cfbc32a119c7337fcf246
Shuvo3078 <155833950+Shuvo3078@users.noreply.github.com>
2024-01-29 00:01:35 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+$('.main_banner_slider').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+  autoplaySpeed: 3000,
+  speed: 1000,
+});
+$('.main_slider').slick({
+    arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: `<i class="fa-solid fa-angle-left left"></i>`,
+    nextArrow: `<i class="fa-solid fa-angle-right right"></i>`,
+    centerMode: true,
+    centerPadding: '0',
+});
